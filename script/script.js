3a6f12be36a13f34dc94ca1ce6428f1ba29adb33
kalbek <access2kaleb@gmail.com>
2023-03-16 14:19:47 +0300

    implement main program section for desktop version

--- script/script.js
+++ script/script.js
@@ -16,16 +16,16 @@ function main() {
       mainText: "Director of Art Centre Nabi and a board member of CC Korea",
       subText:
         "As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.",
-      imageUrl: "../images/home/speaker-02.png",
+      imageUrl: "../images/home/speaker-03.png",
     },
     {
       id: 3,
-      title: "Yochai Benkler",
+      title: "Lila tretikov",
       mainText:
-        "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+        "Director of Art Centre Nabi and a board member of CC Korea",
       subText:
         "As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.",
-      imageUrl: "../images/home/speaker-03.png",
+      imageUrl: "../images/home/speaker-05.png",
     },
     {
       id: 4,
@@ -33,7 +33,7 @@ function main() {
       mainText: "",
       subText:
         "Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012. he was inducted into the inaugural class of the Internet Society’s (ISOC) Internet Hall of Fame",
-      imageUrl: "../images/home/speaker-04.png",
+      imageUrl: "../images/home/speaker-02.png",
     },
     {
       id: 5,
@@ -41,13 +41,13 @@ function main() {
       mainText: "President of Young Pirates of Europe",
       subText:
         "European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July",
-      imageUrl: "../images/home/speaker-05.png",
+      imageUrl: "../images/home/speaker-04.png",
     },
     {
       id: 6,
       title: "Ryan Merkley",
       mainText: "CEO of Creativve Commons, ex COO of the Mozilla Foundation",
-      subText: "",
+      subText: "Ryan had been leading open-source projects at the Mozilla Foundation such as the open-source move-ment",
       imageUrl: "../images/home/speaker-06.png",
     },
   ];
@@ -95,12 +95,19 @@ function main() {
         </div>
         `;
     });
-    //   toogle text content for MORE / LESS button
+    //   toogle text content for MORE / LESS button and flip arrow direction on more or less
     const speakerContainer = document.getElementById("speaker-container");
+    const moreOrLessArrow = document.querySelector(".arrow");
     if (speakerContainer.classList.contains("visible")) {
-      moreOrLess.innerHTML = "LESS";
-    } else moreOrLess.innerHTML = "MORE";
-    //   keep first two speaker visible on LESS
+      {
+        moreOrLess.innerHTML = "LESS";
+        moreOrLessArrow.classList.add("rotate180");
+      }
+    } else {
+      moreOrLess.innerHTML = "MORE";
+      moreOrLessArrow.classList.remove("rotate180");
+    }
+    //   keep first two speakers visible when LESS is selected
     const firstSpeaker = document.querySelector(".speaker1");
     const secondSpeaker = document.querySelector(".speaker2");
     firstSpeaker.classList.remove("hidden");
